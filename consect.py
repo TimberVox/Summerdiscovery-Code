ce6c2381eefdd7820bebad89a0ed014fa909d829
TimberVox <timbervox49@gmail.com>
2023-07-05 09:58:46 -0700

    change of intersection

---
+++ consect.py
@@ -0,0 +1,8 @@
+print ("y1=ax+b and y2=cx+d")
+a=int(input ("what is a "))
+b=int(input ("what is b "))
+c=int(input ("waht is c "))
+d=int(input ("what is d "))
+x=(d-b)/(a-c)
+y=a*x+b
+print ("the y intersect is "+str(y)+" and the x intersect is "+str(x))
