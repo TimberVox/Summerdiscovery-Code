8c97575b7246e541ea24ce0e978149c795417f66
TimberVox <timbervox49@gmail.com>
2023-07-10 14:03:28 -0700

    shibai

---
+++ operation.py/斐波那契数列.py
@@ -0,0 +1,11 @@
+def fibonacci(n):
+    x=n
+    while x>=0:
+        if x==0:
+            fibonacci(x)==1
+        if x==1:
+            fibonacci(x)==1
+    return fibonacci(x)==fibonacci(x-1)+fibonacci(x-2)
+print (fibonacci(6))
+
+    
