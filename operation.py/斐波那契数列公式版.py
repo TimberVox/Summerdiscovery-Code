79f4da672d95246102a09d9187b788b8ef835860
TimberVox <timbervox49@gmail.com>
2023-07-10 14:03:47 -0700

    kaji

---
+++ operation.py/斐波那契数列公式版.py
@@ -0,0 +1,8 @@
+def fibonacci(n):
+    x=n
+    if x==1:
+        fibonacci(x)==1
+    if x>1:
+        fibonacci(x)==1/(5**-2(((1+5**-2)/2)**x)-((1-5**-2)/2)**x)
+    return fibonacci(n)
+print (fibonacci(7))
