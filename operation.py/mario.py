d778e0067efbd2c381682d515a5ec6677c427299
TimberVox <timbervox49@gmail.com>
2023-07-13 11:30:31 -0700

    chenggongle

--- operation.py/mario.py
+++ operation.py/mario.py
@@ -2,14 +2,15 @@ import pygame
 
 key_A=97
 key_D=100
-key_X=120
-
-
+key_W=119
 ground=500
-
-mario_pos=[50,ground]
-mario_size=[50,120]
+mario_pos=[0,ground]
+mario_size=[360,251]
 mario_vel=[0,0]
+
+mario_image = pygame.image.load("/Users/TINTIN/Desktop/oc们/狮鹫2.png")
+mario_image = pygame.transform.scale(mario_image,mario_size)
+mario_image = pygame.transform.flip(mario_image,True,False)
 
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
@@ -26,16 +27,18 @@ while running:
         if event.type==pygame.KEYDOWN:
             if event.key == key_A:
                 mario_vel[0] = -600
+                mario_image = pygame.transform.flip(mario_image,True,False)
             if event.key == key_D:
-                mario_vel[0] = 600
-            if event.key == key_X:
+                mario_vel[0] = 600 
+            if event.key == key_W:
                 mario_vel[1] = -500
         if event.type == pygame.KEYUP:
             if event.key == key_A:
                 mario_vel[0] = 0
+                mario_image = pygame.transform.flip(mario_image,True,False)
             if event.key == key_D:
                 mario_vel[0] = 0
-            if event.key ==key_X:
+            if event.key ==key_W:
                 mario_vel[1] = 0
     
     mario_vel[1] +=1000*dt
@@ -49,8 +52,8 @@ while running:
 
     screen.fill([110,220,250])
 
-    pygame.draw.rect(screen, [150,0,0], mario_pos+mario_size)
-
+    screen.blit(mario_image,mario_pos)
+    
     pygame.display.flip()
     dt = clock.tick(60) / 1000
 
